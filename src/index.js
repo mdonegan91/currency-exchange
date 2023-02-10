08154d1546b836ef240876a7881b2301a4b27239
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-10 12:48:22 -0800

    webpack images

--- src/index.js
+++ src/index.js
@@ -19,18 +19,23 @@ function printCurrency(apiResponse, amount, currSelect) {
   if (currSelect === "JPY") {
     const jpyTotal = (apiResponse.conversion_rates.JPY * amount).toFixed(2);
     document.querySelector('#showResponse').innerText = `$${amount} USD equals ¥${jpyTotal} ${currSelect}`;
+    document.querySelector("img#yen-img").setAttribute("class", "show");
   } else if (currSelect === "EUR") {
     const eurTotal = (apiResponse.conversion_rates.EUR * amount).toFixed(2);
     document.querySelector('#showResponse').innerText = `$${amount} USD equals €${eurTotal} ${currSelect}`;
+    document.querySelector("img#euro-img").setAttribute("class", "show");
   } else if (currSelect === "MXN") {
     const mxnTotal = (apiResponse.conversion_rates.MXN * amount).toFixed(2);
     document.querySelector('#showResponse').innerText = `$${amount} USD equals $${mxnTotal} ${currSelect}`;
+    document.querySelector("img#peso-img").setAttribute("class", "show");
   } else if (currSelect === "CHF") {
     const chfTotal = (apiResponse.conversion_rates.CHF * amount).toFixed(2);
     document.querySelector('#showResponse').innerText = `$${amount} USD equals ₣${chfTotal} ${currSelect}`;
+    document.querySelector("img#franc-img").setAttribute("class", "show");
   } else if (currSelect === "AUD") {
     const audTotal = (apiResponse.conversion_rates.AUD * amount).toFixed(2);
     document.querySelector('#showResponse').innerText = `$${amount} USD equals $${audTotal} ${currSelect}`;
+    document.querySelector("img#aud-img").setAttribute("class", "show");
   }
 }
 
@@ -44,9 +49,23 @@ function handleForm(event) {
   document.querySelector('#currency-amt').value = null;
   const currSelect = document.querySelector('#currency-name').value;
   getCurrency(amount, currSelect);
+  document.getElementById("reset").setAttribute("class", "show");
+  document.getElementById("submit").disabled = true;
+  document.getElementById("reset").disabled = false;
 }
 
+function startOver() {
+  document.getElementById("submit").disabled = false;
+  document.getElementById("reset").disabled = true;
+  document.getElementById("reset").setAttribute("class", "hidden");
+  document.querySelector("img#yen-img").setAttribute("class", "hidden");
+  document.querySelector("img#euro-img").setAttribute("class", "hidden");
+  document.querySelector("img#peso-img").setAttribute("class", "hidden");
+  document.querySelector("img#franc-img").setAttribute("class", "hidden");
+  document.querySelector("img#aud-img").setAttribute("class", "hidden");
+}
 
 window.addEventListener('load', function () {
   document.querySelector('form').addEventListener('submit', handleForm);
+  document.querySelector("button.delete").addEventListener("click", startOver);
 });
